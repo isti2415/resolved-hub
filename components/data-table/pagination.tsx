cf0fe54952b618ea203ec2fc3ae62768f4d07be8
Istiaq Ahmed <connect@istiaqahmed.me>
2024-08-27 00:08:23 +0600

    fixed pagination total count

--- components/data-table/pagination.tsx
+++ components/data-table/pagination.tsx
@@ -46,7 +46,7 @@ import {
           </div>
           <div className="flex w-[100px] items-center justify-center text-sm font-medium">
             Page {table.getState().pagination.pageIndex + 1} of{" "}
-            {table.getPageCount() + 1}
+            {table.getPageCount() === 0? 1 : table.getPageCount()}
           </div>
           <div className="flex items-center space-x-2">
             <Button
